355375c8d467bd10270857bbdb417e58a7f7a54f
i-flower <https://github.com/i-flower/git-.git>
2020-02-05 19:25:28 +0800

    使用element-ui和vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
   <!-- 空的 -->
-  </div>
+  <el-button type="danger" round>危险按钮</el-button>
+  <el-button type="success" disabled>成功按钮</el-button>
+  <el-button type="danger" plain disabled>危险按钮</el-button>
+ </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入路由
+import router from './router/index.js'
+// 1.如果 router/index.js 可以省略router 默认访问index.js文件
+// 2. ./ ../ 去找到对应的目录并不是很准确  使用vue-cli提供的@（相当于src路径）
+import router from '@/router'
+
+// 导入
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 使用饿了么ui
+Vue.use(ElementUI);
+
 // 配置vue框架的日志级别 productTip
 // false 不是生产环境级别日志，开发环境日志，日志详细
 // true 生产环境级别日志，上线之后部署服务器之后，日志简单
@@ -11,7 +23,8 @@ Vue.config.productionTip = false
 // 使用App.vue徐建渲染到 #app容器中
 // (conmponents公用级别组件) (views 页面级别的组件)
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
 
 // main.js 的职责

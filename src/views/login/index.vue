e852ce673b69a14b9690743a91b56856beb8a7b3
i-flower <https://github.com/i-flower/git-.git>
2020-02-06 17:17:29 +0800

    输入框信息校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,15 +2,59 @@
     <!-- <div class='container'> Login </div> -->
     <!-- 全屏容器 -->
     <div class='container-login'>
-       <el-card class="my-card">
+      <el-card class="my-card">
          <img src="../../assets/logo_index.png">
+      
+      <el-form :model="loginForm" :rules="loginRules" >
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:238px;margin-right:10px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item> 
+        <el-form-item> 
+          <el-checkbox :value="true">我已阅读和同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item> 
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+       </el-form>
+
        </el-card>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'app-login'
+    name: 'app-login',
+    data() {
+        const checkMobile = (rule, value, callback) => {
+            if(!/^1[3-9]\d{9}$/.test(value)) {
+               callback(new Error('手机号格式不正确'))
+            } else{
+              callback()   
+            }
+            
+        }
+        return{
+            loginForm: {
+            mobile: '',
+            code: ''
+            },
+            loginRules: {
+            mobile: [
+                {required:true, message:'请输入手机号',trigger:'blur'},
+                //自定义校验规则
+                {validator: checkMobile, trigger: 'blur'}
+            ],
+            code: [
+                {required:true, message:'请输入验证码',trigger:'blur'},
+                {len:6, message:'请输入6位验证码', trigger:'blur'}
+            ]
+            }
+        }
+    }
 }
 </script>
 
@@ -20,7 +64,7 @@ export default {
         height: 100%;
         position:absolute;
         left: 0;
-        right: 0;
+        top: 0;
         background:url(../../assets/login_bg.jpg) no-repeat center;
         background-size: cover;
         .my-card{
@@ -33,7 +77,7 @@ export default {
             img{
                 display: block;
                 width: 200px;
-                margin: 0 auto;
+                margin: 0 auto 20px;
             }
         }
     }

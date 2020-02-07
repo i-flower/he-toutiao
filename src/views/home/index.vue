4f956154974228fcbb90f7e7eb1b87ae2db68e50
i-flower <https://github.com/i-flower/git-.git>
2020-02-07 16:24:35 +0800

    首页-侧边栏折叠效果完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,9 @@
 <template>
   <el-container class="container-home">
     <!-- 侧边栏 -->
-    <el-aside width="200px">
+    <el-aside class="my-aside" :width="isOpen?'200px':'64px'">
         <!-- 侧边栏logo -->
-        <div class="logo"></div>
+        <div class="logo" :class="{minLogo: !isOpen}"></div>
         <!--<el-menu 菜单容器 -->
         <!-- default-active="1" 指定那个菜单被激活 使用index属性来标识 -->
         <!-- background-color="#002033" 菜单容器背景颜色 -->
@@ -14,6 +14,8 @@
            background-color="#002033"
            text-color="#fff"
            active-text-color="#ffd04b"
+           :collapse="!isOpen"
+           :collapse-transition="false"
         >
         <el-menu-item index="1">
           <i class="el-icon-s-home"></i>
@@ -50,7 +52,7 @@
       <!-- 头部导航区域 -->
       <el-header>
         <!-- 设置字体图标 -->
-        <span class="el-icon-s-fold icon"></span>
+        <span @click="toggleMenu()" class="el-icon-s-fold icon"></span>
         <span>江苏传智播客教育科技有限公司</span>
         <!-- 下拉菜单 -->
         <el-dropdown class="my-dropdown" :hide-on-click="false">
@@ -75,7 +77,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'app-home',
+  data () {
+    return {
+      isOpen: true
+    }
+  },
+  methods: {
+    // 切换左菜单
+    toggleMenu () {
+      //切换状态 展开 收起   宽度/logo/导航菜单组件
+      this.isOpen = !this.isOpen
+    }
+  }
+};
 </script>
 
 <style scoped lang="less" >
@@ -91,6 +107,11 @@ export default {};
         width: 100%;
         height: 60px;
         background: #002244 url(../../assets/logo_admin.png) no-repeat center / 140px auto;
+    }
+      // 样式一定需要写在 logo 下方，加上这个类之后去覆盖上面样式
+    .minLogo {
+      background-image: url(../../assets/logo_admin_01.png);
+      background-size: 36px auto; 
     }
     .el-menu {
         border-right: none;

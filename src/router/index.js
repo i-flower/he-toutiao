8ec7a1a71b3a79e350dc401ad634448b51b64957
i-flower <https://github.com/i-flower/git-.git>
2020-02-07 12:32:20 +0800

    首页基础布局

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,14 @@ Vue.use(VueRouter)
 // 导入login组件
 import Login from '@/views/login'
 
+// 导入home组件
+import Home from '@/views/home'
 // 初始化
 const router = new VueRouter({
     routes: [
-    // 登录
-    {path:'/login', component: Login}
+        // 登录
+        { path: '/login', component: Login },
+        { path: '/', component: Home }
     ]
 })
 // 导出
---
+++ src/views/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container class="container-home">
+     <el-aside width="200px">Aside</el-aside>
+     <el-container>
+         <el-header>Header</el-header>
+         <el-main>Main</el-main>
+     </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="less" >
+    .container-home {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        // background-color: skyblue;
+        .el-aside{
+            background-color: #002033;
+        }
+        .el-header{
+            border-bottom:1px solid #ddd;
+        }
+    }
+   
+</style>

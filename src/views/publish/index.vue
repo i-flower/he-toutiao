5173cc03e74491b8bba67e118600dd0163c0f8ce
i-flower <https://github.com/i-flower/git-.git>
2020-02-16 22:12:36 +0800

    发布文章-富文本组件

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -10,22 +10,24 @@
         <el-form-item label="标题：">
           <el-input v-model="articleForm.title" style="width:400px"></el-input>
         </el-form-item>
-        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="内容：">
+          <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
+        </el-form-item>
         <el-form-item label="封面：">
-          <!-- <el-radio-group >
+          <el-radio-group >
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
-          </el-radio-group> -->
+          </el-radio-group>
           <div>封面组件</div>
         </el-form-item>
         <el-form-item label="频道：">
-            <my-channel v-model="articleForm.channel_id"></my-channel>
+          <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary">发表</el-button>
-            <el-button>存入草稿箱</el-button>
+          <el-button type="primary">发表</el-button>
+          <el-button>存入草稿箱</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -33,17 +35,45 @@
 </template>
 
 <script type="text/javascript">
+// 富文本需要的样式
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+// 组件配置对象
+import { quillEditor } from "vue-quill-editor";
 export default {
   name: "app-publish",
+  components: {
+    // 注册局部组件 quill-editor
+    quillEditor
+  },
   data() {
     return {
       // 文章数据
       articleForm: {
         title: null,
-        channel_id: null
+        channel_id: null,
+        content: null
+      },
+
+      editorOption: {
+        // 占位文字
+        placeholder: "",
+        // 模块配置
+        modules: {
+          toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            ["image"]
+          ]
+        }
       }
     };
   }
+  // 配置富文本对象
 };
 </script>
 
